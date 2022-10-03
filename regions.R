8c39124f4518ce6cf70de22227ab0f30a0e8a5cd
justforjan <schneiderjan.2013@gmail.com>
2022-10-03 22:37:19 +0200

    Added filter for continents

--- regions.R
+++ regions.R
@@ -2,13 +2,14 @@ library(tidyverse)
 library(countrycode)
 
 general <- read_csv("D:/Daten/Bildung/HS Mannheim/BA/bachelors-thesis/processed_data/general.csv")
-
-regions <- as.data.frame(unique(general$country))
+regions <- as.data.frame(unique(separate(general, dmu, c("country", "year"), "_", remove=FALSE)$country))
 
 colnames(regions) <- c("country")
+
+regions <- drop_na(regions)
 
 regions$continent <- countrycode(sourcevar = regions$country,
                                   origin = "country.name",
                                   destination = "continent")
 
-write_csv(regions, "regions.csv")
+write_csv(regions, "regions_prev.csv")
